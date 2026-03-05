3d6dbfb55b934415a0100de2d4ad4b333cb6be96
jamorAA <139582763+jamorAA@users.noreply.github.com>
2026-03-05 17:19:02 +0300

    update script.groovy
    Removed buildJar and buildImage functions from script.

--- script.groovy
+++ script.groovy
@@ -1,17 +1,3 @@
-def buildJar() {
-    echo "building the application..."
-    sh "mvn package"
-}
-
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh "docker build -t jamoraa/demo-java-maven-app:2.2 ."
-        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh "docker push jamoraa/demo-java-maven-app:2.2"
-    }
-}
-
 def deployApp() {
     echo "deploying the application..."
 }
